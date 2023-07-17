5da4c3e2e2e245f8cde8946048823d1362c65596
Ana Clara <anaclarapontesm@hotmail.com>
2023-07-16 22:35:10 -0300

    study document properties

---
+++ prop document/main.js
@@ -0,0 +1,16 @@
+// propriedades document
+console.log(document.body)
+
+console.log(document.head)
+
+console.log(document.links)
+
+document.links[0].textContent = "Twitter"
+
+console.log(document.URL)
+
+console.log(document.title)
+
+document.title = "Aula 01"
+
+console.log(document.title)
